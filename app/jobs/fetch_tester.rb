a431ff2477cba63b55147b7bbd362cc1cbc803f6
Dan Bode <bodepd@gmail.com>
2013-03-08 19:36:19 -0800

    add cache to recreate issue

--- app/jobs/fetch_tester.rb
+++ app/jobs/fetch_tester.rb
@@ -1,4 +1,5 @@
 require 'time'
+require 'torquebox-cache'
 
 class FetchTester
     include TorqueBox::Injectors
@@ -7,6 +8,11 @@ class FetchTester
       puts "Fetching..."
       @topic = fetch('/topics/fetcher')
       puts "Got: #{@topic}"
+      @pr_cache   = TorqueBox::Infinispan::Cache.new(
+        :name    => 'pull_requests',
+        # do not persist while I am testing
+        # :persist => '/data/treasure'
+      )
     end
     def run
       puts "Publishing..."
